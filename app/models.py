e405ba448015298d94b007a2e013a38e188d4cb3
ifaryd <coulibalyzieisrael@gmail.com>
2021-02-27 11:36:44 +0000

    sam2

--- app/models.py
+++ app/models.py
@@ -1,19 +1,13 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-# Create your models here.
 class Langue(models.Model):
     field_id = models.AutoField(db_column='_id', primary_key=True)  # Field renamed because it started with '_'.
     initial = models.TextField()
     classe = models.TextField(blank=True, null=True)  # This field type is a guess.
     type_contenu = models.TextField(blank=True, null=True)
     sens_lecture = models.TextField(blank=True, null=True)
-    
     def __str__(self):
         return self.initial
-
     class Meta:
         db_table = 'langue'
 
@@ -28,14 +22,10 @@ class Predication(models.Model):
     duree = models.IntegerField(blank=True, null=True)
     id_langue = models.ForeignKey('Langue', db_column='id_langue', on_delete=models.CASCADE)
     lien_video = models.TextField(blank=True, null= True)
-    
     def __str__(self):
         return self.titre
-
     class Meta:
         db_table = 'predication'
-
-
 
 class Verset(models.Model):  
     field_id = models.AutoField(db_column='_id', primary_key=True)  # Field renamed because it started with '_'.
@@ -44,9 +34,7 @@ class Verset(models.Model):
     id_langue = models.ForeignKey('Langue', db_column='id_langue', on_delete=models.CASCADE)
     id_pred = models.ForeignKey('Predication', db_column='id_pred', on_delete=models.CASCADE)
     #id_parab = models.ForeignKey('Parabole', on_delete=models.CASCADE)
-    
     def __str__(self):
         return self.contenu
-
     class Meta:
         db_table = 'verset'
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def index(request):
+    #predication = models.Predication.objects.language('fr')[:1].get()
+    #verset = models.Verset.objects.language(predication.get_current_language())[:1].get()
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def predications_lists(request):
+    return render(request, 'predications-lists.html')
