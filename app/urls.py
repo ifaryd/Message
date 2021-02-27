7df1736bec72cb050e8b54585f68ad3e72d8f4c5
Soro08 <soro@nan.ci>
2021-02-27 16:55:38 +0000

    update code

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact', views.contact, name='contact'),
-    path('predications', views.predications_lists, name="predications")
+    path('', views.accueil, name='accueil'),
+    path('<slug:lang>/', views.index, name='index'),
+    path('<slug:lang>/contact', views.contact, name='contact'),
+    path('<slug:lang>/predications', views.predications_lists, name="predications"),
+    path('<slug:lang>/predication/<int:predid>', views.predications_detail, name="predication")
 ]
